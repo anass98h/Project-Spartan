9c4db3fceeaa50db59ef403553e76bde30507a98
raspbianlike <raspbian@gmx.at>
2017-08-06 23:12:29 +0200

    MAde the resolver button shit

--- src/SpartanGUI/Tabs/hvhtab.cpp
+++ src/SpartanGUI/Tabs/hvhtab.cpp
@@ -169,12 +169,16 @@ void HvH::RenderTab() {
     {
         ImGui::BeginChild(XORSTR("HVH2"), ImVec2(0, 0), true);
         {
+            ImGui::Checkbox("Resolve All", &Settings::Resolver::resolveAll::enabled);
+            if (Settings::Resolver::resolveAll)
+            { 
             ImGui::Text(XORSTR("Resolver"));
             ImGui::Separator();
             ImGui::Combo("##HUGTYPE", (int*) & Settings::Resolver::Hugtype, Hugtypes, IM_ARRAYSIZE(Hugtypes));
             ImGui::SliderFloat("##HUGTICKS", &Settings::Resolver::ticks, 0, 16, "Ticks: %0.f");
             ImGui::SliderFloat("##HUGMODULO", &Settings::Resolver::modulo, 0, 16, "Modulo: %0.f");
             ImGui::Separator();
+            } 
             ImGui::Text("Misc");
             ImGui::Checkbox("LBY Indicator", &Settings::lbyindicator::enabled);
             ImGui::Separator();
