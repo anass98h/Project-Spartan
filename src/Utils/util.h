9e1e56799633c20cee3f8246ef6a6ab6009ef471
Marc Steiner <git@marcsteiner.me>
2017-08-28 11:26:53 +0200

    Fix missing PicoSha2 class (used for Protection SHA256 hashes)

--- src/Utils/util.h
+++ src/Utils/util.h
@@ -10,6 +10,9 @@
 #include <locale>
 #include <codecvt>
 #include <sstream>
+#include <ctime>
+#include <iostream>
+#include <fstream>
 #include "../ImGUI/imgui.h"
 #include "../SDK/SDK.h"
 
@@ -29,6 +32,9 @@ namespace Util
 	Color GetHealthColor(C_BasePlayer* player);
 	int RandomInt(int min, int max);
 	long GetEpochTime();
+	const char* GetCurrentTimeAsISO8601Timestamp();
+	const char* GetSHA256(const char* input);
+	const char* GetSHA256(std::ifstream stream);
 
 	const std::map<int,int> * GetModelTypeBoneMap(C_BasePlayer* player);
 	ModelType GetModelTypeID(C_BasePlayer* player);
