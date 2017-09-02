5fb0ce18f604e146b51a9f64fef90e61c808ea4c
Apostrophe <chrynexx@gmail.com>
2017-09-02 19:56:39 +0200

    posFix

--- src/SpartanGUI/Spartangui.cpp
+++ src/SpartanGUI/Spartangui.cpp
@@ -144,12 +144,19 @@ void SetupMainMenuBar() {
 
 
 
+        ImVec2 center;
 
-
-        if (ImGui::IsMouseClicked(1, true)|| ImGui::IsMouseClicked(2,true))
+        if (ImGui::IsMouseClicked(1, true)){
             ImGui::OpenPopup("##piepopup");
 
-        ImVec2 center = ImGui::GetIO().MouseClickedPos[1];
+            center = ImGui::GetIO().MouseClickedPos[1];
+        }
+        if(ImGui::IsMouseClicked(2,true)){
+         ImGui::OpenPopup("##piepopup");
+
+            center = ImGui::GetIO().MouseClickedPos[2];
+        }
+        
         int n = PiePopupSelectMenu(center, "##piepopup", items, items_count);
         switch (n){ // a switch for my love Myrrib 
         
