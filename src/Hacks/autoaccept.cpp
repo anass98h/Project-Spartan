1066475ebd9a0281babe8efc5051fa9b1f3049fa
Myrrib <martin.czhk@gmail.com>
2017-09-02 10:21:20 +0200

    Autoaccept fix?
    https://github.com/LWSS/Fuzion/commit/cf6745c61143cbae2bd3ffe18fd3e06b87b7caa2

--- src/Hacks/autoaccept.cpp
+++ src/Hacks/autoaccept.cpp
@@ -15,7 +15,7 @@ void AutoAccept::PlaySound(const char* filename)
 	if (engine->IsInGame())
 		return;
 
-	if (strcmp(filename, XORSTR("UI/competitive_accept_beep.wav")) != 0) // new wav file on autoaccept, thanks @flawww
+	if (strcmp(filename, XORSTR("!UI/competitive_accept_beep.wav")) != 0) // https://github.com/LWSS/Fuzion/commit/cf6745c61143cbae2bd3ffe18fd3e06b87b7caa2
 		return;
 
 	CServerConfirmedReservationCheckCallback empty_callback;
