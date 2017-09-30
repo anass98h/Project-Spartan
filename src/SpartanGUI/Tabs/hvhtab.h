46fd880119a08a03bb460d2283149059ec029b02
myrrib <martin.czhk@gmail.com>
2017-09-30 20:37:39 +0200

    little fix

--- src/SpartanGUI/Tabs/hvhtab.h
+++ src/SpartanGUI/Tabs/hvhtab.h
@@ -5,7 +5,7 @@
 namespace HvH {
 
     static const char* yTypes[] = {
-            "No AA", "Forwards", "Backwards", "Spin", "Fake Sideways", "Backjitter", "FJitter", "LBY Jitter", "LBY Spin", "LBY", "LBY Breaker", "Fake LBY", "LBY on Ground", "MasterLooser" //LBY Based
+            "No AA", "Forwards", "Backwards", "Spin", "Fake Sideways", "Backjitter", "FJitter", "LBY Jitter", "LBY Spin", "LBY", "LBY Breaker", "Fake LBY", "LBY on Ground", "MasterLooser", //LBY Based
                     "Tank 1", "Tank 2", "Tank 2", "Myrrib's eap", "Richi's eap", "Rasp 1", "Rasp 2", "FeetWiggle", "Legit Trolling 1", "Legit Trolling 2", "Custom 1", "Custom 2" // Other
     };
     static const char* lbyTypes[] = { "Breaker 1", "Breaker 2", "Breaker 3", "Avoz", "Myrrib", "None" };
