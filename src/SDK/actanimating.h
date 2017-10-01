2510884434d609cd3aca2f332729c7030a4f6093
myrrib <martin.czhk@gmail.com>
2017-10-01 18:41:20 +0200

    sdk

--- src/SDK/actanimating.h
+++ src/SDK/actanimating.h
@@ -3,17 +3,12 @@
 #ifndef ACTANIMATING_H
 #define ACTANIMATING_H
 
-class CActAnimating : public CBaseAnimating
+class CActAnimating
 {
 public:
-	DECLARE_CLASS( CActAnimating, CBaseAnimating );
 
 	void			SetActivity( Activity act );
 	inline Activity	GetActivity( void ) { return m_Activity; }
-
-	virtual int	ObjectCaps( void ) { return BaseClass::ObjectCaps() & ~FCAP_ACROSS_TRANSITION; }
-
-	DECLARE_DATADESC();
 
 private:
 	Activity	m_Activity;
