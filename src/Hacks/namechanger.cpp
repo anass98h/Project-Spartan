3d17bbf6749d6c8e6d65e533d5220a607846c704
Apostrophe <apostropheqq@gmail.com>
2017-10-09 18:53:45 +0200

    namespam memes :feelscringe:

--- src/Hacks/namechanger.cpp
+++ src/Hacks/namechanger.cpp
@@ -3,6 +3,7 @@
 std::string NameChanger::origName = "";
 int NameChanger::changes = -1;
 NameChanger::NC_Type NameChanger::type = NC_Type::NC_NORMAL;
+NameChanger::nnType NameChanger::type2 = nnType::Spartan;
 NameChanger::Colors NameChanger::color = Colors::LIGHT_RED;
 std::vector<std::pair<NameChanger::Colors, const char*>> NameChanger::colors = {
         { NameChanger::Colors::WHITE,        "White" },
@@ -22,6 +23,13 @@ std::vector<std::pair<NameChanger::Colors, const char*>> NameChanger::colors = {
         { NameChanger::Colors::RED,          "Red" },
         { NameChanger::Colors::ORANGE,       "Orange" },
 };
+std::vector<std::pair<NameChanger::nnType, const char*>> NameChanger::nntypes = {
+        { NameChanger::nnType::Spartan,   "Spartan" },
+        { NameChanger::nnType::Aimtux,    "Aimtux" },
+        { NameChanger::nnType::Interwebz, "Interwebz" },
+        { NameChanger::nnType::Aimware,   "Aimware" },
+};
+bool switcher = false;
 
 static std::string GetName() {
     IEngineClient::player_info_t playerInfo;
@@ -44,8 +52,7 @@ void NameChanger::AyywareCrash() {
 
     std::stringstream ss;
 
-    for( auto i = 0; i<=150; i++ )
-    {
+    for ( auto i = 0; i <= 150; i++ ) {
         ss << static_cast<char>(-1);
         //Breaks it and doesnt work thanks marc
         //const char char_max = (char)(((unsigned char) char(-1)) / 2);
@@ -75,6 +82,15 @@ void NameChanger::InitColorChange( NameChanger::NC_Type type,
     NameChanger::origName = GetName();
     NameChanger::type = type;
     NameChanger::color = color;
+}
+
+void NameChanger::InitNoName( NameChanger::NC_Type type, NameChanger::nnType type2 ) {
+
+    NameChanger::changes = 0;
+    NameChanger::origName = GetName();
+    NameChanger::type = type;
+    NameChanger::type2 = type2;
+
 }
 
 void NameChanger::BeginFrame( float frameTime ) {
@@ -107,9 +123,54 @@ void NameChanger::BeginFrame( float frameTime ) {
     if ( changes >= 5 ) {
         switch ( NameChanger::type ) {
             case NC_Type::NC_NORMAL:
-                SetName( Util::PadStringRight( XORSTR( "\230\nProject Spartan" ),
-                                               strlen( XORSTR( "\230\nProject Spartan" ) ) +
-                                               Util::RandomInt( 10, 50 ) ) );
+                switch ( NameChanger::type2 ) {
+
+                    case nnType::Spartan:
+                        if ( !switcher ) {
+
+                            SetName(Util::PadStringRight( XORSTR( "\230Project Spartan" ), strlen( XORSTR( "Project Spartan" ) ) + Util::RandomInt( 10, 50 ) ) );
+                            switcher = !switcher;
+                        } else {
+
+                            SetName( Util::PadStringRight( XORSTR( "\230cantvac.me" ),
+                                                           strlen( XORSTR( "\230cantvac.me" ) ) +
+                                                           Util::RandomInt( 10, 50 ) ) );
+                            switcher = !switcher;
+                        }
+
+                        break;
+                    case nnType::Aimtux:
+                        SetName( Util::PadStringRight( XORSTR( "\230AIMTUX.NET" ),
+                                                       strlen( XORSTR( "\230AIMTUX.NET" ) ) +
+                                                       Util::RandomInt( 10, 50 ) ) );
+                        break;
+                    case nnType::Interwebz:
+                        if ( !switcher ) {
+
+                            SetName( XORSTR( "-INTERWEBZ" ) );
+                            switcher = !switcher;
+                        } else {
+
+                            SetName(  XORSTR( "INTERWEBZ-" ));
+                            switcher = !switcher;
+                        }
+
+                        break;
+                    case nnType::Aimware:
+                        if ( !switcher ) {
+
+                            SetName( XORSTR( "\xe1\xab\xa8 AIMWARE.net" ));
+                            switcher = !switcher;
+                        } else {
+
+                            SetName(  XORSTR( "\xe1\xab\xa5 AIMWARE.net" ));
+                            switcher = !switcher;
+                        }
+
+                        break;
+
+                }
+
                 break;
             case NC_Type::NC_SOLID:
                 SetName( Util::PadStringRight( Colorize( origName, NameChanger::color ),
@@ -121,7 +182,59 @@ void NameChanger::BeginFrame( float frameTime ) {
 
         return;
     }
+    if ( NameChanger::type != NC_Type::NC_RAINBOW ){
+        switch ( NameChanger::type2 ) {
 
-    //SetName( Util::PadStringRight( XORSTR( "\nProject Spartan" ), strlen( XORSTR( "\nProject Spartan" ) ) +
-    //                                                             changes ) ); //THis cheat needs a website :^)
+            case nnType::Spartan:
+                if ( !switcher ) {
+
+                    SetName( Util::PadStringRight( XORSTR( "Project Spartan" ),
+                                                   strlen( XORSTR( "Project Spartan" ) ) +
+                                                           changes ) );
+                    switcher = !switcher;
+                } else {
+
+                    SetName( Util::PadStringRight( XORSTR( "cantvac.me" ),
+                                                   strlen( XORSTR( "cantvac.me" ) ) +
+                                                           changes ) );
+                    switcher = !switcher;
+                }
+
+                break;
+            case nnType::Aimtux:
+                SetName( Util::PadStringRight( XORSTR( "AIMTUX.NET" ),
+                                               strlen( XORSTR( "AIMTUX.NET" ) ) +
+                                                       changes ) );
+                break;
+            case nnType::Interwebz:
+                if ( !switcher ) {
+
+                    SetName(  XORSTR( "-INTERWEBZ" ) );
+                    switcher = !switcher;
+                } else {
+
+                    SetName(  XORSTR( "INTERWEBZ-" ) );
+                    switcher = !switcher;
+                }
+
+                break;
+            case nnType::Aimware:
+                if ( !switcher ) {
+
+                    SetName(  XORSTR( "\xe1\xab\xa8 AIMWARE.net" ) );
+                    switcher = !switcher;
+                } else {
+
+                    SetName(  XORSTR( "\xe1\xab\xa5 AIMWARE.net" ) );
+                    switcher = !switcher;
+                }
+
+                break;
+
+
+        }
+
+
+
+    }
 }
