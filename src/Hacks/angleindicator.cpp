fd91dfcf69495d0b476dbaac761f89b59e0ade0c
myrrib <martin.czhk@gmail.com>
2017-10-21 14:08:47 +0200

    Added LBY into Angle Indicator

--- src/Hacks/angleindicator.cpp
+++ src/Hacks/angleindicator.cpp
@@ -59,18 +59,24 @@ void AngleIndicator::PaintImGui() {
     text2.append( fakeText );
 
     std::ostringstream stream3;
-    stream3 << Math::RoundFloat( velocity );
-    std::string veloText( stream3.str() );
-    std::string text3( XORSTR( "Velocity: " ) );
-    text3.append( veloText );
-    // Calculation of text position on screen
+    stream3 << Math::RoundFloat( *pLocal->GetLowerBodyYawTarget() );
+    std::string lbyText( stream3.str() );
+    std::string text3( XORSTR( "LBY: " ) );
+    text3.append( lbyText );
 
     std::ostringstream stream4;
-    stream4 << (AntiAim::IsStanding() ? XORSTR("Standing") : (AntiAim::IsMoving() ? XORSTR("Moving") :
+    stream4 << Math::RoundFloat( velocity );
+    std::string veloText( stream4.str() );
+    std::string text4( XORSTR( "Velocity: " ) );
+    text4.append( veloText );
+    // Calculation of text position on screen
+
+    std::ostringstream stream5;
+    stream5 << (AntiAim::IsStanding() ? XORSTR("Standing") : (AntiAim::IsMoving() ? XORSTR("Moving") :
                                                       (AntiAim::IsAirborne() ? XORSTR("Airborne") : XORSTR("Unknown"))));
-    std::string stateText( stream4.str() );
-    std::string text4(XORSTR("Current State: "));
-    text4.append(stateText);
+    std::string stateText( stream5.str() );
+    std::string text5(XORSTR("Current State: "));
+    text5.append(stateText);
 
     int width;
     int height;
@@ -84,13 +90,17 @@ void AngleIndicator::PaintImGui() {
     int textY3 = height - ( height - ( height * 55 / 100 ) );
     int textX4 = ( width - ( width - ( width * 85 / 100 ) ) );
     int textY4 = height - ( height - ( height * 57 / 100 ) );
+    int textX5 = ( width - ( width - ( width * 85 / 100 ) ) );
+    int textY5 = height - ( height - ( height * 59 / 100 ) );
     Draw::ImText( ImVec2( textX, textY ), ImColor( 255, 255, 255 ), text.c_str(), NULL, 0.0f, NULL,
                   ImFontFlags_Shadow );
     Draw::ImText( ImVec2( textX2, textY2 ), ImColor( 255, 255, 255 ), text2.c_str(), NULL, 0.0f, NULL,
                   ImFontFlags_Shadow );
+    Draw::ImText( ImVec2( textX3, textY3 ), ImColor( 255, 255, 255 ), text3.c_str(), NULL, 0.0f, NULL,
+                  ImFontFlags_Shadow );
     if ( Settings::AngleIndicator::Veloc ) {
-        Draw::ImText( ImVec2( textX3, textY3 ), ImColor( 255, 255, 255 ), text3.c_str(), NULL, 0.0f, NULL,
+        Draw::ImText( ImVec2( textX4, textY4 ), ImColor( 255, 255, 255 ), text4.c_str(), NULL, 0.0f, NULL,
                       ImFontFlags_Shadow );
-        Draw::ImText( ImVec2( textX4, textY4 ), ImColor(255, 255, 255), text4.c_str(), NULL, 0.0f, NULL, ImFontFlags_Shadow);
+        Draw::ImText( ImVec2( textX5, textY5 ), ImColor(255, 255, 255), text5.c_str(), NULL, 0.0f, NULL, ImFontFlags_Shadow);
     }
 }
