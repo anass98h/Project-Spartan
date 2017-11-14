ebc5ca6ea449e7472f6633d7abe4c141f3430783
Apostropheqq <apostropheqq@gmail.com>
2017-11-14 19:20:54 +0100

    all right corporal marc , sir !

--- src/SpartanGUI/Tabs/misctab.cpp
+++ src/SpartanGUI/Tabs/misctab.cpp
@@ -216,10 +216,10 @@ void Misc::RenderTab() {
                         ImGui::SliderFloat(XORSTR("##TPCAMOFFSET"), &Settings::ThirdPerson::distance, 0.0f, 500.0f);
                         ImGui::PopItemWidth();
                         UI::KeyBindButton(&Settings::ThirdPerson::key);
-                        ImGui::Text("transparency (Scoped)");
+                        ImGui::Text("Transparency (Scoped)");
                         ImGui::PushItemWidth(-1);
                         ImGui::SliderFloat(XORSTR("##TRANSPARENCY"), &Settings::ThirdPerson::transparency, 0.1f , 0.6f ,
-                        XORSTR("Amount %0.1f"));
+                        XORSTR("%0.1f"));
                         ImGui::PopItemWidth();
                     }
                     ImGui::Columns(1);
